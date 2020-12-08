772243016c68750c3ccbc621977ef8f8ae18108e
j-tooyoung <arnoldtooyoung@hotmail.com>
2020-12-08 23:58:07 +0800

    submit lc842

--- backtrack/lc842.py
+++ backtrack/lc842.py
@@ -2,5 +2,28 @@ from typing import List
 
 
 class Solution:
-    def splitIntoFinbonacci(self, S:str) -> List[int]:
-        return
+    def splitIntoFinbonacci(self, S: str) -> List[int]:
+        ans = list()
+
+        def backtrack(index: int):
+            if index == len(S):
+                return len(ans) >= 3
+            cur = 0
+            for i in range(index, len(S)):
+                if i > index and S[index] == '0':
+                    break
+                cur = cur * 10 + ord(S[i]) - ord("0")
+                if cur > 2 ** 31 - 1:
+                    break
+
+                if len(ans) < 2 or cur == ans[-2] + ans[-1]  :
+                    ans.append(cur)
+                    if backtrack(i + 1):
+                        return True
+                    ans.pop()
+                elif len(ans) > 2 and ans[-2] + ans[-1] < cur:
+                    break
+            return False
+
+        backtrack(0)
+        return ans
