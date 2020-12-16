9604e16919ced809548d62a98ca4ef84ebb9ec75
xpfh <xupeng4215@fiberhome.com>
2020-12-16 10:25:40 +0800

    submit lc290

--- hash/lc290.py
+++ hash/lc290.py
@@ -8,12 +8,16 @@ class Solution:
         if len(pattern) != len(words):
             return False
         for ch, word in zip(pattern, words):
-            if ch not in ch2word:
-                ch2word[ch] = word
-            elif ch2word[ch] != word:
+            # if ch not in ch2word:
+            #     ch2word[ch] = word
+            # elif ch2word[ch] != word:
+            #     return False
+            # if word not in word2ch:
+            #     word2ch[word] = ch
+            # elif word2ch[word] != ch:
+            #     return False
+            if (ch in ch2word and ch2word[ch] != word) or (word in word2ch and word2ch[word] != ch):
                 return False
-            if word not in word2ch:
-                word2ch[word] = ch
-            elif word2ch[word] != ch:
-                return False
+            ch2word[ch] = word
+            word2ch[word] = ch
         return True
